52f11b166175ef3c9821ef9775a548b07d6b28de
Dheeraj <dheeraj.iiser@gmail.com>
2022-11-25 00:04:58 +0530

    added nth deri updated readme

--- seaborn_tutorial.py
+++ seaborn_tutorial.py
@@ -8,10 +8,8 @@ fmri=sns.load_dataset("fmri")
 #sns.relplot(data=fmri,kind="line",x="timepoint",y="signal",col="region",hue="event",style="event")
 #plt.show()
 
+#  Distributional plots
+sns.displot(data=tips,x="total_bill",col="time",kde=True)
 
-import random
-x=[i for i in range(100)]
-y=[i**2 + random.random() for i in x]
 
-sns.relplot(x=x,y=y,kind="line")
 plt.show()
